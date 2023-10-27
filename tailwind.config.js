f2f656fe96623a8ef981f94059373c1ebdcbb293
zarxy <zarxymen@gmail.com>
2023-10-27 16:43:30 +0300

    refactor: add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+   content: ["./src/**/*.{js,jsx,ts,tsx}",],
+   theme: {
+      extend: {
+         colors: {
+            'bg-green': '#b5e9dd',
+            'point-green': '#00b587',
+         },
+      },
+   },
+   plugins: [],
+}
+
